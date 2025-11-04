b5e4741ed0cc3667dc300bf16f071dd2d9ed7063
Tomasz Herman <tomekh2015@gmail.com>
2025-11-04 11:56:20 +0100

    Add image field to Airplane model with upload functionality

--- airport/models.py
+++ airport/models.py
@@ -1,6 +1,17 @@
+import os
+import uuid
+
 from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils.text import slugify
+
+
+def airplane_image_file_path(instance, filename):
+    """Generate file path for airplane image"""
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.name)}-{uuid.uuid4()}{extension}"
+    return os.path.join("uploads/airplanes/", filename)
 
 
 class Airport(models.Model):
@@ -68,6 +79,11 @@ class Airplane(models.Model):
         AirplaneType,
         on_delete=models.CASCADE,
         related_name="airplanes"
+    )
+    image = models.ImageField(
+        null=True,
+        blank=True,
+        upload_to=airplane_image_file_path
     )
 
     class Meta:
